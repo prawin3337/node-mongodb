998928018aa34eb5a4a2ea096bbeb2cbd86b27a5
Pravin Bhosale <pbhosale@redseal.net>
2025-01-23 15:02:43 +0530

    Update and delete req

--- app.js
+++ app.js
@@ -5,21 +5,22 @@ const { ObjectId } = require('mongodb');
 
 const app = express();
 
+// convert req body to json
+app.use(express.json());
+
 // db connection
-let db;
 connectToDb((err) => {
     if(!err) {
         app.listen(3000, () => {
             console.log('App listening on port 3000');
         });
-        db = getDb();
     }
 })
 
 // routes
 app.get('/books', (req, res) => {
     let books = [];
-    db.collection('books')
+    getDb().collection('books')
         .find()
         .sort({ author: 1})
         .forEach(book => books.push(book))
@@ -40,7 +41,7 @@ app.get('/books/:id', (req, res) => {
         return;
     }
     
-    db.collection('books')
+    getDb().collection('books')
         .findOne({_id: new ObjectId(id)})
         .then(doc => {
             res.status(200).json(doc)
@@ -48,5 +49,53 @@ app.get('/books/:id', (req, res) => {
         .catch(() => {
             console.log(err);
             res.status(500).json({ error: "Could not fetch the docuemnts." })
+        })
+});
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+    getDb().collection('books')
+        .insertOne(book)
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(() => {
+            console.log(err);
+            res.status(500).json({ error: "Could not add the docuemnts." })
+        })
+});
+
+app.delete('/books/:id', (req, res) => {
+    const id = req.params.id;
+    if(!ObjectId.isValid(id)) {
+        res.status(500).json({ error: "Could not delete the docuemnts. Invalid id." });
+        return;
+    }
+    getDb().collection('books')
+        .deleteOne({_id: new ObjectId(id)})
+        .then(result => {
+            res.status(200).json(result)
+        })
+        .catch(() => {
+            console.log(err);
+            res.status(500).json({ error: "Could not delete the docuemnts." })
+        })
+});
+
+app.patch('/books/:id', (req, res) => {
+    const id = req.params.id;
+    if (!ObjectId.isValid(id)) {
+        res.status(500).json({ error: "Could not delete the docuemnts. Invalid id." });
+        return;
+    }
+    const data = req.body;
+    getDb().collection('books')
+        .updateOne({ _id: new ObjectId(id) }, { $set: data })
+        .then((result) => {
+            res.status(200).json(result)
+        })
+        .catch(() => {
+            console.log(err);
+            res.status(500).json({ error: "Could not update the docuemnts." })
         })
 })
